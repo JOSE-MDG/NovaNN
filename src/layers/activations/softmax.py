b387f4c33b9b939804256b0c99c13ad6c8117039
JOSE-MDG <josepemlengineer@gmail.com>
2025-11-07 14:53:38 -0800

    Softmax backward pass successfully inplemented

--- src/layers/activations/softmax.py
+++ src/layers/activations/softmax.py
@@ -7,12 +7,17 @@ class SoftMax(Activation):
     def __init__(self):
         super().__init__()
         self.affect_init = False
+        self.out = None
 
     def forward(self, x: np.ndarray) -> np.ndarray:
         logits_max = np.max(x, axis=1, keepdims=True)
         stable_logits = x - logits_max
         logits_exp = np.exp(stable_logits)
         logits_sum = np.sum(logits_exp, axis=1, keepdims=True)
-        return logits_exp / logits_sum
+        self.out = logits_exp / logits_sum
+        return self.out
 
-    def backward(self, grad: np.ndarray) -> np.ndarray: ...
+    def backward(self, grad: np.ndarray) -> np.ndarray:
+        s = np.sum(self.out * grad, axis=1, keepdims=True)
+        grad = self.out * (grad - s)
+        return grad
