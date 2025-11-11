cb35bb670fb3d6820adc804dd40672bd12a17a1d
JOSE-MDG <josepemlengineer@gmail.com>
2025-11-11 13:35:11 -0800

    A note file was created to record the results of this implementation compared to other Deep Learning frameworks

---
+++ main.py
@@ -0,0 +1,6 @@
+import src.model.nn as nn
+import src.losses.functional as F
+
+from src.layers.linear.linear import Linear
+from src.layers.activations.relu import ReLU
+from src.optim.adam import Adam
