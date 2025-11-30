7b16431cb28f4690b67ce52a3466692d5bca6d3c
JOSE-MDG <josepemlengineer@gmail.com>
2025-11-30 13:23:19 +0100

    Renaming folder name

---
+++ src/layers/convolutional/conv.py
@@ -0,0 +1,15 @@
+import numpy as np
+from src.module import Layer, Parameters
+
+
+class Conv1d(Layer):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int,
+        stride: int = 1,
+        padding: int = 0,
+    ):
+        super().__init__()
+        ...
