996cda548487a3ae5165b6577ed7048ebef2cb27
JOSE-MDG <josepemlengineer@gmail.com>
2025-12-02 19:38:15 +0100

    Decorative funcrion was documented

--- novann/core/decorators.py
+++ novann/core/decorators.py
@@ -4,12 +4,18 @@ from novann.core import logger
 
 
 def measure_exc_time(func):
+    """Decorator function to measure the execution time of a function."""
+
     @wraps(func)
     def wrapper(*args, **kwargs):
+        """Wraps the original function"""
+
+        # start time execution
         start = time.perf_counter()
         reusul = func(*args, **kwargs)
         end = time.perf_counter()
+
         logger.info(f"Execution time of '{func.__name__}' was {round(end - start)}s")
-        del reusul
+        del reusul  # remove the result of the function
 
     return wrapper
