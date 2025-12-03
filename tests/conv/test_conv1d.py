1ab70ae7bdcd52ff6b2ad3df0aa71bc2fbc26522
JOSE-MDG <josepemlengineer@gmail.com>
2025-12-03 21:42:13 +0100

    Tests methods were created

--- tests/conv/test_conv1d.py
+++ tests/conv/test_conv1d.py
@@ -1,2 +1,9 @@
 import numpy as np
 import pytest
+from novann.layers import Conv1d
+
+
+def test_forward_and_backward_shape(): ...
+
+
+def test_numeric_grad(): ...
--- tests/conv/test_conv2d.py
+++ tests/conv/test_conv2d.py
@@ -1,2 +1,10 @@
 import numpy as np
 import pytest
+
+from novann.layers import Conv2d
+
+
+def test_forward_and_backward_shape(): ...
+
+
+def test_numeric_grad(): ...
