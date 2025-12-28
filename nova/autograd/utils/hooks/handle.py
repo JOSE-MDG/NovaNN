9ac66bfbf6c280554068eec92ece3ae6152ce99f
JOSE-MDG <josepemlengineer@gmail.com>
2025-12-28 14:20:01 +0100

    fix: aliases between args

--- nova/autograd/utils/hooks/handle.py
+++ nova/autograd/utils/hooks/handle.py
@@ -2,15 +2,21 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from nova._typing import Hook
+    from nova._typing import Hook, StepHook
 
 
 class HooksHandle:
-    def __init__(self, hooks_list: list[Hook], hook_func: Hook):
-        self.hooks_list: list[Hook] = hooks_list
-        self.hooks_func: Hook = hook_func
+    def __init__(
+        self,
+        hooks_list: list[Hook] | list[StepHook] | list[Hook | StepHook],
+        hook_func: Hook | StepHook,
+    ):
+        self.hooks_list: list[Hook] | list[StepHook] | list[Hook | StepHook] = (
+            hooks_list
+        )
+        self.hooks_func: Hook | StepHook = hook_func
         self._removed: bool = False
 
-    def remove(self):
+    def remove(self) -> None:
         if not self._removed and len(self.hooks_list) > 0:
-            self.hooks_list.remove(self.hooks_func)
+            self.hooks_list.remove(self.hooks_func)  # type: ignore
