1d88cdc97c7e6b8d82d7187ae5d7b5eaf51b6e37
JOSE-MDG <josepemlengineer@gmail.com>
2025-12-29 17:48:27 +0100

    fix(nn): prevent float64 promotion by casting eps and momentum to input dtype

--- nova/nn/modules/batchnorm.py
+++ nova/nn/modules/batchnorm.py
@@ -34,7 +34,7 @@ Batch Normalization Usage Cheat Sheet:
 """
 
 
-class BatchNorm1d(Module):
+class _BatchNorm(Module):
     def __init__(
         self,
         num_features: int,
@@ -88,7 +88,7 @@ class BatchNorm1d(Module):
             if self.num_batches_tracked is not None:
                 self.num_batches_tracked += 1
                 if self.momentum is None:
-                    exp_avg_factor = 1.0 / self.num_batches_tracked.to(nova.double)
+                    exp_avg_factor = 1.0 / float(self.num_batches_tracked.item())
                 else:
                     exp_avg_factor = self.momentum
 
@@ -104,141 +104,19 @@ class BatchNorm1d(Module):
         )
 
 
-class BatchNorm2d(Module):
-    def __init__(
-        self,
-        num_features: int,
-        momentum: Optional[float] = 0.1,
-        eps: float = 1e-5,
-        affine: bool = True,
-        track_running_stats: bool = True,
-        dtype: Optional[Dtype] = None,
-    ) -> None:
-        super().__init__()
+class BatchNorm1d(_BatchNorm):
+    """Applies Batch Normalization over a 2D or 3D input"""
 
-        self.track_running_stats = track_running_stats
-        self.num_features = num_features
-        self.momentum = momentum
-        self.affine = affine
-        self.eps = eps
-
-        if self.track_running_stats:
-            self.running_mean: Buffer = Buffer(nova.empty((num_features,), dtype=dtype))
-            self.running_var: Buffer = Buffer(nova.empty((num_features,), dtype=dtype))
-            self.num_batches_tracked: Buffer = Buffer(nova.empty(0, dtype=nova.long))
-        else:
-            self.register_buffer("running_mean", None)
-            self.register_buffer("running_var", None)
-            self.register_buffer("num_batches_tracked", None)
-
-        if self.affine:
-            self.weight: Parameter = Parameter(nova.empty((num_features,), dtype=dtype))
-            self.bias: Parameter = Parameter(nova.empty((num_features,), dtype=dtype))
-        else:
-            self.register_parameter("weight", None)
-            self.register_parameter("bias", None)
-
-        self.reset_parameters()
-
-    def reset_parameters(self) -> None:
-
-        if self.track_running_stats:
-            self.running_mean.zero_()
-            self.running_var.ones_()
-            self.num_batches_tracked.zero_()
-
-        if self.affine:
-            init.ones_(self.weight)
-            init.zeros_(self.bias)
-
-    def forward(self, input: Tensor) -> Tensor:
-        exp_avg_factor = 0.0
-
-        if self._training and self.track_running_stats:
-            if self.num_batches_tracked is not None:
-                self.num_batches_tracked += 1
-                if self.momentum is None:
-                    exp_avg_factor = 1.0 / self.num_batches_tracked.to(nova.double)
-                else:
-                    exp_avg_factor = self.momentum
-
-        return F.batch_norm(
-            input=input,
-            running_mean=self.running_mean,
-            running_var=self.running_var,
-            weight=self.weight,
-            bias=self.bias,
-            training=self._training or not self.track_running_stats,
-            momentum=exp_avg_factor,
-            eps=self.eps,
-        )
+    ...
 
 
-class BatchNorm3d(Module):
-    def __init__(
-        self,
-        num_features: int,
-        momentum: Optional[float] = 0.1,
-        eps: float = 1e-5,
-        affine: bool = True,
-        track_running_stats: bool = True,
-        dtype: Optional[Dtype] = None,
-    ) -> None:
-        super().__init__()
+class BatchNorm2d(_BatchNorm):
+    """Applies Batch Normalization over a 4D input"""
 
-        self.track_running_stats = track_running_stats
-        self.num_features = num_features
-        self.momentum = momentum
-        self.affine = affine
-        self.eps = eps
+    ...
 
-        if self.track_running_stats:
-            self.running_mean: Buffer = Buffer(nova.empty((num_features,), dtype=dtype))
-            self.running_var: Buffer = Buffer(nova.empty((num_features,), dtype=dtype))
-            self.num_batches_tracked: Buffer = Buffer(nova.empty(0, dtype=nova.long))
-        else:
-            self.register_buffer("running_mean", None)
-            self.register_buffer("running_var", None)
-            self.register_buffer("num_batches_tracked", None)
 
-        if self.affine:
-            self.weight: Parameter = Parameter(nova.empty((num_features,), dtype=dtype))
-            self.bias: Parameter = Parameter(nova.empty((num_features,), dtype=dtype))
-        else:
-            self.register_parameter("weight", None)
-            self.register_parameter("bias", None)
+class BatchNorm3d(_BatchNorm):
+    """Applies Batch Normalization over a 5D input"""
 
-        self.reset_parameters()
-
-    def reset_parameters(self) -> None:
-
-        if self.track_running_stats:
-            self.running_mean.zero_()
-            self.running_var.ones_()
-            self.num_batches_tracked.zero_()
-
-        if self.affine:
-            init.ones_(self.weight)
-            init.zeros_(self.bias)
-
-    def forward(self, input: Tensor) -> Tensor:
-        exp_avg_factor = 0.0
-
-        if self._training and self.track_running_stats:
-            if self.num_batches_tracked is not None:
-                self.num_batches_tracked += 1
-                if self.momentum is None:
-                    exp_avg_factor = 1.0 / self.num_batches_tracked.to(nova.double)
-                else:
-                    exp_avg_factor = self.momentum
-
-        return F.batch_norm(
-            input=input,
-            running_mean=self.running_mean,
-            running_var=self.running_var,
-            weight=self.weight,
-            bias=self.bias,
-            training=self._training or not self.track_running_stats,
-            momentum=exp_avg_factor,
-            eps=self.eps,
-        )
+    ...
