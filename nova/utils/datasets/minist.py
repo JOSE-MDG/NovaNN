e44acfea3ed530b5324890ecba4ae6af8f31b742
JOSE-MDG <josepemlengineer@gmail.com>
2025-12-30 18:01:58 +0100

    feat(datasets): The design was adapted to the data loading methods

--- nova/utils/datasets/minist.py
+++ nova/utils/datasets/minist.py
@@ -1,5 +1,9 @@
+from __future__ import annotations
+import nova
+import traceback
 import numpy as np
 import pandas as pd
+from typing import Optional, TYPE_CHECKING
 from nova.utils.log_config import logger
 from nova.utils.data import normalize, split_features_and_labels
 from nova.core import (
@@ -8,14 +12,19 @@ from nova.core import (
     MNIST_VALIDATION_DATA_PATH,
 )
 
+if TYPE_CHECKING:
+    from nova._typing import Dtype, TensorsOrArrays
+
 
 def load_mnist_data(
+    do_normalize: bool = True,
+    tensor4d: bool = False,
+    as_tensor: bool = True,
+    dtype: Optional[Dtype] = None,
     train_path: str = EXPORTATION_MNIST_TRAIN_DATA_PATH,
     test_path: str = MNIST_TEST_DATA_PATH,
     val_path: str = MNIST_VALIDATION_DATA_PATH,
-    do_normalize: bool = True,
-    tensor4d: bool = False,
-):
+) -> TensorsOrArrays:
     """
     Load MNIST dataset from CSV files and optionally normalize it.
     Args:
@@ -56,17 +65,26 @@ def load_mnist_data(
             std = np.std(x_train, axis=0) + 1e-8
 
             # Apply normalization with training stastistics
-            x_train_norm = normalize(x_train, mean, std).astype(np.float32)
-            x_test_norm = normalize(x_test, mean, std).astype(np.float32)
-            x_val_norm = normalize(x_val, mean, std).astype(np.float32)
+            x_train = normalize(x_train, mean, std).astype(dtype)
+            x_test = normalize(x_test, mean, std).astype(dtype)
+            x_val = normalize(x_val, mean, std).astype(dtype)
 
             # Return normalized datasets
-            return (x_train_norm, y_train), (x_test_norm, y_test), (x_val_norm, y_val)
-        else:
-            # Return raw datasets
-            return (x_train, y_train), (x_test, y_test), (x_val, y_val)
+
+        if as_tensor:
+            x_train = nova.tensor(x_train, dtype=dtype)
+            y_train = nova.tensor(y_train, dtype=dtype)
+
+            x_test = nova.tensor(x_test, dtype=dtype)
+            y_test = nova.tensor(y_test, dtype=dtype)
+
+            x_val = nova.tensor(x_val, dtype=dtype)
+            y_val = nova.tensor(y_val, dtype=dtype)
+
+        return ((x_train, y_train), (x_test, y_test), (x_val, y_val))
 
     # Handle exceptions during data loading
     except Exception as e:
-        logger.error(f"Error loading MNIST data: {e}")
-        raise
+        lines = [line for line in traceback.format_exception(e)]
+        logger.error(f"Error loading MNIST data \n")
+        print(*lines)
