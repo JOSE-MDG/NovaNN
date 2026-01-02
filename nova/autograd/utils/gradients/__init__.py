df1a27a399a5278e3b288b49b49faf75dd63c05e
JOSE-MDG <josepemlengineer@gmail.com>
2026-01-02 12:28:23 +0100

    feat(gradients): grad_checking.py file was moved to gradients dir

--- nova/autograd/utils/gradients/__init__.py
+++ nova/autograd/utils/gradients/__init__.py
@@ -1,4 +1,4 @@
 from .clipping import clip_grad_value_, clip_grad_norm_
-from .checking import grad_check_wrt_inputs
+from .grad_checking import grad_check_wrt_inputs
 
 __all__ = ["clip_grad_value_", "clip_grad_norm_", "grad_check_wrt_inputs"]
--- nova/autograd/utils/gradients/checking/__init__.py
+++
@@ -1,4 +0,0 @@
-from .grad_checking import grad_check_wrt_inputs
-
-
-__all__ = ["grad_check_wrt_inputs"]
