48e336908d27ae27dc21b09d3a729c814555dcf9
JOSE-MDG <josepemlengineer@gmail.com>
2026-01-02 11:08:05 +0100

    fix(grad): NoneType problem in arguments fixed

--- nova/autograd/grad.py
+++ nova/autograd/grad.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from numpy import ndarray
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 
 if TYPE_CHECKING:
     from nova import Tensor
@@ -9,12 +9,9 @@ if TYPE_CHECKING:
 def grad(
     outputs: Tensor | list[Tensor],
     inputs: Tensor | list[Tensor],
-    gradients: Tensor | ndarray | list[Tensor | ndarray],
+    gradients: Optional[Tensor | ndarray | list[Tensor | ndarray]] = None,
     retain_grads: bool = True,
 ) -> list[ndarray] | ndarray:
-
-    if None in inputs or None in outputs:
-        raise ValueError("The gradient of a None type cannot be calculated")
 
     if not isinstance(inputs, list):
         inputs = [inputs]
@@ -25,15 +22,7 @@ def grad(
     if gradients is not None and not isinstance(gradients, (list, tuple)):
         gradients = [gradients]
 
-    tensor_ids = {}  # to avoid the loss by memory reference
-    id = 0
-
-    for input in inputs:
-        if input.grad is not None:
-            tensor_ids[input] = id
-            id += 1
-
-    prev_grads = {tensor_ids[input]: input.grad for input in inputs}
+    prev_grads = {input: input.grad for input in inputs}
 
     for input in inputs:
         input.zero_grad(set_to_none=True)
@@ -45,7 +34,7 @@ def grad(
     grads = [inp.grad for inp in inputs]
 
     if retain_grads:
-        for i, input in enumerate(inputs):
-            input.grad = prev_grads[i]
+        for input in inputs:
+            input.grad = prev_grads[input]
 
     return grads[0] if len(grads) == 1 else grads
