7c7b8753ae2116fecef8e015e928d58ca72c0fb3
JOSE-MDG <josepemlengineer@gmail.com>
2026-01-05 21:25:11 +0100

    config.py info

---
+++ nova/core/config.py
@@ -0,0 +1,6 @@
+"""
+This module has not yet been started because the layer
+initialization criteria have not been defined, nor has it been
+determined whether automatic layer initialization with activation-based
+parameters will be implemented.
+"""
