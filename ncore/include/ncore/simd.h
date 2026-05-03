6e052339da530a4b2ccb253e39cb3e42453a01ca
JOSE-MDG <josepemlengineer@gmail.com>
2026-05-03 02:28:01 +0200

    feat(simd): implement thread-safe CPUID-based feature detection

---
+++ ncore/include/ncore/simd.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <stdbool.h>
+
+typedef struct {
+  bool sse4_2_;
+
+  bool avx_;
+  bool avx2_;
+  bool avx2_int8_;
+  bool avx2_vnni_;
+  bool f16c_;
+
+  bool vnni_;
+
+  bool fma3_;
+
+  bool avx512f_;
+  bool avx512_vnni_;
+  bool avx512_fp16_;
+  bool avx512_bf16_;
+
+  bool amx_fp16_;
+  bool amx_bf16_;
+  bool amx_int8_;
+} Capabilities_;
+
+const Capabilities_ *get_cpu_capabilites();
---
+++ ncore/src/core/simd.c
@@ -0,0 +1,92 @@
+#include <ncore/simd.h>
+#include <stdio.h>
+#include <threads.h>
+
+#ifdef _WIN32
+#include <intrin.h>
+#else
+#include <cpuid.h>
+#endif
+
+#include <string.h>
+
+typedef unsigned int uint;
+
+static Capabilities_ Caps_;
+static once_flag init_flag = ONCE_FLAG_INIT;
+
+static inline void detect_cpu_capabilities_(Capabilities_ *__restrict caps) {
+  memset(caps, 0, sizeof(Capabilities_));
+
+  uint eax, ebx, ecx, edx;
+
+#ifdef _WIN32
+  int cpu_info[4];
+  __cpuid(cpu_info, 1);
+  eax = (uint)cpu_info[0];
+  ebx = (uint)cpu_info[1];
+  ecx = (uint)cpu_info[2];
+  edx = (uint)cpu_info[3];
+#else
+  if (!__get_cpuid(1, &eax, &ebx, &ecx, &edx)) {
+    return;
+  }
+#endif
+
+  caps->sse4_2_ = (ecx & (1 << 20)) != 0;
+  caps->fma3_ = (ecx & (1 << 12)) != 0;
+  caps->avx_ = (ecx & (1 << 28)) != 0;
+  caps->f16c_ = (ecx & (1 << 29)) != 0;
+
+#ifdef _WIN32
+  __cpuidex(cpu_info, 7, 0);
+  eax = (uint)cpu_info[0];
+  ebx = (uint)cpu_info[1];
+  ecx = (uint)cpu_info[2];
+  edx = (uint)cpu_info[3];
+#else
+  __cpuid_count(7, 0, eax, ebx, ecx, edx);
+#endif
+
+  caps->avx2_ = (ebx & (1 << 5)) != 0;
+  caps->avx512f_ = (ebx & (1 << 16)) != 0;
+  caps->avx512_vnni_ = (ecx & (1 << 11)) != 0;
+  caps->amx_bf16_ = (edx & (1 << 22)) != 0;
+  caps->avx512_fp16_ = (edx & (1 << 23)) != 0;
+  caps->amx_int8_ = (edx & (1 << 25)) != 0;
+
+#ifdef _WIN32
+  __cpuidex(cpu_info, 7, 1);
+  eax = (uint)cpu_info[0];
+  ebx = (uint)cpu_info[1];
+  ecx = (uint)cpu_info[2];
+  edx = (uint)cpu_info[3];
+#else
+  __cpuid_count(7, 1, eax, ebx, ecx, edx);
+#endif
+
+  caps->avx2_vnni_ = (eax & (1 << 4)) != 0;
+  caps->avx512_bf16_ = (eax & (1 << 5)) != 0;
+  caps->amx_fp16_ = (eax & (1 << 21)) != 0;
+  caps->avx2_int8_ = (edx & (1 << 4)) != 0;
+
+  caps->vnni_ = (bool)(caps->avx512_vnni_ || caps->avx2_vnni_);
+}
+
+static inline void init_once() { detect_cpu_capabilities_(&Caps_); }
+
+const Capabilities_ *get_cpu_capabilities() {
+  call_once(&init_flag, init_once);
+  return &Caps_;
+}
+
+int main() {
+  const Capabilities_ *caps = (const Capabilities_ *)get_cpu_capabilities();
+
+  (caps->sse4_2_) ? printf("SSE4.2: True \n")
+                  : printf("SSE4.2: False\n"); // True
+  (caps->avx512f_) ? printf("AVX512F: True\n")
+                   : printf("AVX512F: False\n"); // False
+
+  return 0;
+}
