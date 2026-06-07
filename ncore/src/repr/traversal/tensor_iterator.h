bef204dc469bd76d275d03154a1a027a35b5cf12
JOSE-MDG <josepemlengineer@gmail.com>
2026-06-08 00:08:07 +0200

    refactor(repr): use coords_t typedef for coordinate arrays

--- ncore/src/repr/traversal/tensor_iterator.h
+++ ncore/src/repr/traversal/tensor_iterator.h
@@ -15,41 +15,49 @@
 
 #pragma once
 
+#include <ncore/headeronly/tensor_utils.h>
 #include <ncore/tensor.h>
 #include <stdbool.h>
 #include <stddef.h>
 
 /**
+ * @struct TensorIterator
  * @brief Iterator state.
  */
 typedef struct {
-  const Tensor *tensor;         ///< The tensor being iterated.
-  size_t coords[NOVA_MAX_DIMS]; ///< Current multi-dimensional coordinate.
-  size_t linear_index;          ///< Element count from start.
-  bool done;                    ///< True when all elements have been visited.
+  const Tensor *tensor; ///< The tensor being iterated.
+  coords_t coords;      ///< Current multi-dimensional coordinate.
+  size_t linear_index;  ///< Element count from start.
+  bool done;            ///< True when all elements have been visited.
 } TensorIterator;
 
 /**
  * @brief Initialise an iterator at the first element.
- * @param it  Uninitialised struct.
- * @param ten Tensor to iterate.
+ *
+ * @param[out] it  Uninitialised struct.
+ * @param[in]  ten Tensor to iterate.
  */
 void iter_init(TensorIterator *it, const Tensor *ten);
 
 /**
  * @brief Advance to the next element in row-major order.
+ *
+ * @param[in] it Iterator to advance.
  */
 void iter_advance(TensorIterator *it);
 
 /**
  * @brief Compute the byte offset of the current element.
  *
+ * @param[in] it Iterator.
  * @return Byte offset into the tensor's data buffer.
  */
 size_t iter_byte_offset(const TensorIterator *it);
 
 /**
  * @brief Check whether iteration is complete.
+ *
+ * @param[in] it Iterator.
  * @return true if all elements have been visited.
  */
 bool iter_done(const TensorIterator *it);
