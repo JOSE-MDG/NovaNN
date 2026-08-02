c3cb73c6239fb64c83feed2473a941d9732bde05
JOSE-MDG <josepemlengineer@gmail.com>
2026-08-02 23:16:47 +0200

    refactor(ffi): re-export NovaError and NovaStatus from bindings
    - Replace DeviceStatus re-exports with NovaError and NovaStatus

--- ncore/rust/src/ffi/cpp.rs
+++ ncore/rust/src/ffi/cpp.rs
@@ -7,6 +7,6 @@
 pub mod bindings;
 
 pub use bindings::{
-    DeviceBuffer, DeviceKind, DeviceMemcpyKind, DeviceStatus, deviceRelease, deviceReserve,
-    deviceResize, getDeviceBackend,
+    DeviceBuffer, DeviceKind, DeviceMemcpyKind, NovaError, NovaStatus, deviceRelease,
+    deviceReserve, deviceResize, getDeviceBackend,
 };
